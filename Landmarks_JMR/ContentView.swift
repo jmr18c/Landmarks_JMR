240c1218de149be4e5c6b5f997cdde6d262b8202
jmr18c <jmr18c@acu.edu>
2020-11-08 20:00:38 -0600

    Creating and Combining Views Complete!!

---
+++ Landmarks_JMR/ContentView.swift
@@ -0,0 +1,43 @@
+//
+//  ContentView.swift
+//  Landmarks_JMR
+//
+//  Created by Justin Raitz on 11/8/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            MapView()
+                .frame(height: 300)
+                .edgesIgnoringSafeArea(.top)
+            
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            
+            VStack(alignment: .leading) {
+                Text("Turtle Rock")
+                    .font(.title)
+                HStack(alignment: .top) {
+                    Text("Joshua Tree National Park")
+                        .font(.subheadline)
+                    Spacer()
+                    Text("California")
+                        .font(.subheadline)
+                }
+            }
+            .padding()
+            
+            Spacer()
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Landmarks_JMR/Landmarks_JMRApp.swift
@@ -0,0 +1,17 @@
+//
+//  Landmarks_JMRApp.swift
+//  Landmarks_JMR
+//
+//  Created by Justin Raitz on 11/8/20.
+//
+
+import SwiftUI
+
+@main
+struct Landmarks_JMRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
